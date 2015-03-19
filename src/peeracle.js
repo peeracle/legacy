1861155258d041d1c4b0c00dfb5b5e7407a37f0c
Axel Isouard <a.isouard@dotstar.fr>
2015-03-20 02:05:09 +0900

    Load Metadata and Utils in node

--- src/peeracle.js
+++ src/peeracle.js
@@ -3,7 +3,9 @@
 (function () {
   var Peeracle = {};
   Peeracle.Media = require('./media');
+  Peeracle.Metadata = require('./metadata');
   Peeracle.Peer = require('./peer');
   Peeracle.Tracker = require('./tracker');
+  Peeracle.Utils = require('./utils');
   module.exports = Peeracle;
 })();
