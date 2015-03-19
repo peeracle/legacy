990d50e57a07cd1add2be562ab66eedf758fea6e
Axel Isouard <a.isouard@dotstar.fr>
2015-03-19 21:04:09 +0900

    Load the media module inside node

--- src/peeracle.js
+++ src/peeracle.js
@@ -2,6 +2,7 @@
 
 (function () {
   var Peeracle = {};
+  Peeracle.Media = require('./media');
   Peeracle.Peer = require('./peer');
   Peeracle.Tracker = require('./tracker');
   module.exports = Peeracle;
