b7679e14dfab95ae308c5740b9231b9fb06abd0a
Axel Isouard <a.isouard@dotstar.fr>
2015-03-22 18:10:14 +0900

    Load the new modules (File, Metadata (Un)serializer) into node

--- src/peeracle.js
+++ src/peeracle.js
@@ -2,8 +2,11 @@
 
 (function () {
   var Peeracle = {};
+  Peeracle.File = require('./file');
   Peeracle.Media = require('./media');
   Peeracle.Metadata = require('./metadata');
+  Peeracle.MetadataSerializer = require('./metadata.serializer');
+  Peeracle.MetadataUnserializer = require('./metadata.unserializer');
   Peeracle.Peer = require('./peer');
   Peeracle.Tracker = require('./tracker');
   Peeracle.Utils = require('./utils');
