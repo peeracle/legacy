b54a652bdb41697db63acbb163cf974a8188b769
Axel Isouard <a.isouard@dotstar.fr>
2015-05-05 18:09:49 +0900

    Add karma sauce launcher

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,32 @@
 module.exports = function (config) {
   'use strict';
 
+  var customLaunchers = {
+    sl_chrome: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Windows 7',
+      version: '35'
+    },
+    sl_firefox: {
+      base: 'SauceLabs',
+      browserName: 'firefox',
+      version: '30'
+    },
+    sl_ios_safari: {
+      base: 'SauceLabs',
+      browserName: 'iphone',
+      platform: 'OS X 10.9',
+      version: '7.1'
+    },
+    sl_ie_11: {
+      base: 'SauceLabs',
+      browserName: 'internet explorer',
+      platform: 'Windows 8.1',
+      version: '11'
+    }
+  };
+
   config.set({
     basePath: './',
 
@@ -16,7 +42,13 @@ module.exports = function (config) {
 
     frameworks: ['jasmine'],
 
-    browsers: ['Chrome', 'Firefox'],
+    sauceLabs: {
+      testName: 'Peeracle Web Unit Tests'
+    },
+    customLaunchers: customLaunchers,
+    browsers: Object.keys(customLaunchers),
+    reporters: ['dots', 'saucelabs'],
+    singleRun: true,
 
     plugins: [
       'karma-chrome-launcher',
