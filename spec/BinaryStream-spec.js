1604176334a28f9292e5d38b48f7987c72b67aec
Axel Isouard <a.isouard@dotstar.fr>
2015-05-05 20:32:11 +0900

    Write basic BinaryStream unit tests, with bytes and strings functions

--- spec/BinaryStream-spec.js
+++ spec/BinaryStream-spec.js
@@ -23,19 +23,84 @@
 /* eslint-disable */
 'use strict';
 
-if (!Peeracle) {
+if (typeof Peeracle === 'undefined') {
   var Peeracle = require('../peeracle');
 }
 
 describe('BinaryStream', function () {
-  var buffer;
-  var binaryStream;
-  it('should create a Uint8Array with 32 zeroes', function () {
+  var buffer = new Uint8Array(32);
+  var binaryStream = new Peeracle.BinaryStream(buffer);
+  var byte;
+  var bytes;
+  var string;
+
+  it('should create a Uint8Array', function () {
     buffer = new Uint8Array(32);
+    for (var i = 0; i < 32; ++i) {
+      buffer[i] = 0;
+    }
     expect(buffer).not.toBeNull();
     expect(buffer.length).toEqual(32);
   });
-  it('should create a new BinaryStream from that array', function () {
-    binaryStream = new Peeracle.BinaryStream(buffer);
+
+  it('should write a random byte', function () {
+    byte = Peeracle.Utils.trunc(Math.random() * 255);
+    binaryStream.writeByte(byte);
+  });
+
+  it('should read a zero', function () {
+    var result = binaryStream.readByte();
+    expect(result).toEqual(0);
+  });
+
+  it('should read the random byte', function () {
+    var result;
+
+    binaryStream.seek(0);
+    result = binaryStream.readByte();
+    expect(result).toEqual(byte);
+  });
+
+  it('should write some random bytes', function () {
+    var string = Math.random().toString(36).slice(2);
+
+    bytes = new Uint8Array(string.length);
+    for (var i = 0, j = string.length; i < j; ++i) {
+      bytes[i] = string.charCodeAt(i);
+    }
+
+    binaryStream.seek(0);
+    binaryStream.writeBytes(bytes);
+  });
+
+  it('should read the random bytes', function () {
+    var result;
+
+    binaryStream.seek(0);
+    result = binaryStream.readBytes(bytes.length);
+    expect(result).toEqual(bytes);
+  });
+
+  it('should write a random string', function () {
+    string = Math.random().toString(36).slice(2);
+
+    binaryStream.seek(0);
+    binaryStream.writeString(string);
+  });
+
+  it('should read the random string', function () {
+    var result;
+
+    binaryStream.seek(0);
+    result = binaryStream.readString();
+    expect(result).toEqual(string);
+  });
+
+  it('should read the two first bytes of the random string', function () {
+    var result;
+
+    binaryStream.seek(0);
+    result = binaryStream.readString(2);
+    expect(result).toEqual(string.slice(0, 2));
   });
 });
