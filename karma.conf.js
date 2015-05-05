35939b4d45608387195d5357edb54ce814e74abb
Axel Isouard <a.isouard@dotstar.fr>
2015-05-05 20:31:14 +0900

    Use karma exclusively locally

--- karma.conf.js
+++ karma.conf.js
@@ -1,26 +1,11 @@
 module.exports = function (config) {
   'use strict';
-
-  var customLaunchers = {
-    sl_chrome: {
-      base: 'SauceLabs',
-      browserName: 'chrome',
-      platform: 'Windows 7',
-      version: '40'
-    },
-    sl_firefox: {
-      base: 'SauceLabs',
-      browserName: 'firefox',
-      version: '37'
-    }
-  };
-
   config.set({
     basePath: './',
 
     files: [
       {
-        pattern: 'dist/peeracle-0.0.1.min.js',
+        pattern: 'dist/peeracle-0.0.1.js',
         included: true
       },
       'spec/**/*-spec.js'
@@ -30,25 +15,23 @@ module.exports = function (config) {
 
     frameworks: ['jasmine'],
 
-    sauceLabs: {
-      testName: 'Peeracle Web Unit Tests'
-    },
-    customLaunchers: customLaunchers,
-    browsers: Object.keys(customLaunchers),
-    reporters: ['dots', 'saucelabs'],
-    singleRun: true,
+    browsers: ['Chrome', 'Firefox'],
 
     plugins: [
       'karma-chrome-launcher',
       'karma-firefox-launcher',
-      'karma-sauce-launcher',
       'karma-jasmine',
-      'karma-junit-reporter'
+      'karma-junit-reporter',
+      'karma-verbose-reporter'
     ],
 
+    reporters: ['verbose', 'junit'],
+
     junitReporter: {
-      outputFile: 'test_out/unit.xml',
+      outputFile: 'unit.xml',
       suite: 'unit'
-    }
+    },
+
+    singleRun: true
   });
 };
