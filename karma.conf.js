abbbdbf5a6b1b5345846633603ca37071aecb6e1
Axel Isouard <a.isouard@dotstar.fr>
2015-05-05 18:19:17 +0900

    Add karma sauce plugin

--- karma.conf.js
+++ karma.conf.js
@@ -6,24 +6,12 @@ module.exports = function (config) {
       base: 'SauceLabs',
       browserName: 'chrome',
       platform: 'Windows 7',
-      version: '35'
+      version: '40'
     },
     sl_firefox: {
       base: 'SauceLabs',
       browserName: 'firefox',
-      version: '30'
-    },
-    sl_ios_safari: {
-      base: 'SauceLabs',
-      browserName: 'iphone',
-      platform: 'OS X 10.9',
-      version: '7.1'
-    },
-    sl_ie_11: {
-      base: 'SauceLabs',
-      browserName: 'internet explorer',
-      platform: 'Windows 8.1',
-      version: '11'
+      version: '37'
     }
   };
 
@@ -53,6 +41,7 @@ module.exports = function (config) {
     plugins: [
       'karma-chrome-launcher',
       'karma-firefox-launcher',
+      'karma-sauce-launcher',
       'karma-jasmine',
       'karma-junit-reporter'
     ],
