1d0295ab7b38dd3f7bc70491b95a25e2b0a056fb
Axel Isouard <a.isouard@dotstar.fr>
2015-05-17 15:16:19 +0900

    Check the argument type in fetchBytes method and constructor

--- spec/DataSource.Http-spec.js
+++ spec/DataSource.Http-spec.js
@@ -27,17 +27,19 @@ var url;
 var length;
 var bytes;
 
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+
 if (typeof Peeracle === 'undefined') {
   var Peeracle = require('../index');
   var http = require('http');
   var i;
 
   url = 'http://127.0.0.1:9990/';
-  length = Math.trunc(Math.random() * 128 + 32);
+  length = Math.floor(Math.random() * 128 - 32 + 1) + 32;
   bytes = new Uint8Array(length);
 
   for (i = 0; i < length; ++i) {
-    bytes[i] = Math.trunc(Math.random() * 255);
+    bytes[i] = Math.floor(Math.random() * 255);
   }
 
   http.createServer(function (request, response) {
@@ -69,7 +71,12 @@ if (typeof Peeracle === 'undefined') {
   }).listen(9990);
 } else {
   url = 'http://out.dotstar.fr/media/dash/' +
-    'tears_of_steel_video_160x72_500K_m.webm';
+    'tears_of_steel_video_160x72_250K.webm';
+  bytes = new Uint8Array([26, 69, 223, 163, 159, 66, 134, 129, 1,
+    66, 247, 129, 1, 66, 242, 129, 4, 66, 243, 129,
+    8, 66, 130, 132, 119, 101, 98, 109, 66, 135, 129,
+    2, 66]);
+  length = 19993725;
 }
 
 describe('Peeracle.DataSource.Http', function () {
@@ -80,22 +87,22 @@ describe('Peeracle.DataSource.Http', function () {
   });
 
   describe('constructor', function () {
-    it('should throw an error on invalid argument', function () {
-      var err = 'Argument must be a string';
+    it('should throw an error on invalid first argument', function () {
+      var err = 'first argument must be a string';
       expect(function () {
-        new Peeracle.DataSource.Http(undefined)
+        new Peeracle.DataSource.Http(undefined);
       }).toThrowError(err);
       expect(function () {
-        new Peeracle.DataSource.Http(0)
+        new Peeracle.DataSource.Http(0);
       }).toThrowError(err);
       expect(function () {
-        new Peeracle.DataSource.Http([])
+        new Peeracle.DataSource.Http([]);
       }).toThrowError(err);
       expect(function () {
-        new Peeracle.DataSource.Http(null)
+        new Peeracle.DataSource.Http(null);
       }).toThrowError(err);
       expect(function () {
-        new Peeracle.DataSource.Http(new Uint8Array(2))
+        new Peeracle.DataSource.Http(new Uint8Array(2));
       }).toThrowError(err);
     });
   });
@@ -113,7 +120,7 @@ describe('Peeracle.DataSource.Http', function () {
     });
     it('should read random bytes', function () {
       var before = http.offset;
-      var position = Peeracle.Utils.trunc(Math.random() * length + 16);
+      var position = Math.floor(Math.random() * length - 16 + 1) + 16;
       http.read(position);
       expect(http.offset).toEqual(before + position);
     });
@@ -147,7 +154,7 @@ describe('Peeracle.DataSource.Http', function () {
       expect(http.offset).toEqual(2);
     });
     it('should seek at random', function () {
-      var position = Peeracle.Utils.trunc(Math.random() * length + 16);
+      var position = Math.floor(Math.random() * (length - 16 + 1)) + 16;
       http.seek(position);
       expect(http.offset).toEqual(position);
     });
@@ -174,26 +181,33 @@ describe('Peeracle.DataSource.Http', function () {
   describe('fetchBytes', function () {
     it('should fetch some bytes', function (done) {
       http.fetchBytes(1, function (result) {
-        expect(result.length).toEqual(1);
+        expect(result).not.toBeNull();
+        expect(result).toEqual(jasmine.any(Uint8Array));
         expect(result[0]).toEqual(bytes[0]);
+        expect(http.length).toEqual(length);
         done();
       });
     });
     it('should fetch the next two bytes', function (done) {
       http.read(1);
       http.fetchBytes(2, function (result) {
-        expect(result.length).toEqual(2);
+        expect(result).not.toBeNull();
+        expect(result).toEqual(jasmine.any(Uint8Array));
         expect(result[0]).toEqual(bytes[1]);
         expect(result[1]).toEqual(bytes[2]);
+        expect(http.length).toEqual(length);
         done();
       });
     });
     it('should fetch some next random bytes', function (done) {
-      var flen = Peeracle.Utils.trunc(Math.random() * length + 16);
+      var flen = Math.floor(Math.random() * (bytes.length + 16 + 1));
+      var sub = bytes.subarray(3, 3 + flen);
       http.read(3);
       http.fetchBytes(flen, function (result) {
+        expect(result).not.toBeNull();
         expect(result.length).toEqual(flen);
-        expect(result).toEqual(bytes.subarray(3, 3 + flen));
+        expect(result).toEqual(sub);
+        expect(http.length).toEqual(length);
         done();
       });
     });
@@ -201,9 +215,61 @@ describe('Peeracle.DataSource.Http', function () {
       http = new Peeracle.DataSource.Http(url + 'invalidstuff');
       http.fetchBytes(1, function (result) {
         expect(result).toBeNull();
+        expect(http.length).toEqual(-1);
         done();
       });
     });
-    it('should throw invalid argument exception');
+    it('should throw invalid first argument exception', function () {
+      var erra = 'first argument must be a number';
+      var errb = 'first argument must be greater than zero';
+      expect(function () {
+        http.fetchBytes(null, null);
+      }).toThrowError(erra);
+      expect(function () {
+        http.fetchBytes([], null);
+      }).toThrowError(erra);
+      expect(function () {
+        http.fetchBytes(new Uint8Array(2), null);
+      }).toThrowError(erra);
+      expect(function () {
+        http.fetchBytes('hey', function (bytes) {
+        });
+      }).toThrowError(erra);
+      expect(function () {
+        http.fetchBytes(null, function (bytes) {
+        });
+      }).toThrowError(erra);
+      expect(function () {
+        http.fetchBytes([], function (bytes) {
+        });
+      }).toThrowError(erra);
+      expect(function () {
+        http.fetchBytes(new Uint8Array(2), function (bytes) {
+        });
+      }).toThrowError(erra);
+      expect(function () {
+        http.fetchBytes('hey', function (bytes) {
+        });
+      }).toThrowError(erra);
+      expect(function () {
+        http.fetchBytes(-10, function (bytes) {
+        });
+      }).toThrowError(errb);
+    });
+    it('should throw invalid first argument exception', function () {
+      var erra = 'second argument must be a callback';
+      expect(function () {
+        http.fetchBytes(1, 'hey');
+      }).toThrowError(erra);
+      expect(function () {
+        http.fetchBytes(2, null);
+      }).toThrowError(erra);
+      expect(function () {
+        http.fetchBytes(3, []);
+      }).toThrowError(erra);
+      expect(function () {
+        http.fetchBytes(3, new Uint8Array(2));
+      }).toThrowError(erra);
+    });
   });
 });
